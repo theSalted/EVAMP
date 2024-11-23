31c53c030927c64557d0fac24787d1e0600bae73
Yuhao Chen <yuhao.chen2000@icloud.com>
2024-11-22 17:20:20 -0800

    support for two more pins

--- SuperHangOnReader/FinalLevelTopDownMqap.sketch/FinalLevelTopDownMqap.sketch.ino
+++ SuperHangOnReader/FinalLevelTopDownMqap.sketch/FinalLevelTopDownMqap.sketch.ino
@@ -1,10 +1,14 @@
 int analogPin0 = A0;
 int analogPin1 = A1;
 int analogPin2 = A2;
+int analogPin3 = A3;
+int analogPin4 = A4;
 
 int analogValue0 = 0;
 int analogValue1 = 0;
 int analogValue2 = 0;
+int analogValue3 = 0;
+int analogValue4 = 0;
 
 const float referenceVoltage = 5.0;
 
@@ -18,11 +22,15 @@ void loop() {
   analogValue0 = analogRead(analogPin0);
   analogValue1 = analogRead(analogPin1);
   analogValue2 = analogRead(analogPin2);
+  analogValue3 = analogRead(analogPin3);
+  analogValue4 = analogRead(analogPin4);
 
   // Convert analog readings to voltage
   float voltage0 = (analogValue0 * referenceVoltage) / 1023.0;
   float voltage1 = (analogValue1 * referenceVoltage) / 1023.0;
   float voltage2 = (analogValue2 * referenceVoltage) / 1023.0;
+  float voltage3 = (analogValue3 * referenceVoltage) / 1023.0;
+  float voltage4 = (analogValue4 * referenceVoltage) / 1023.0;
 
   // Print the analog values as voltage to the serial monitor
   Serial.print(voltage0);
@@ -30,5 +38,9 @@ void loop() {
   Serial.print(voltage1);
   Serial.print(",");
   Serial.println(voltage2);
+  Serial.print(",");
+  Serial.println(voltage3);
+  Serial.print(",");
+  Serial.println(voltage4);
 
 }
--- SuperHangOnReader/sketch_nov7a/sketch_nov7a.ino
+++ SuperHangOnReader/sketch_nov7a/sketch_nov7a.ino
@@ -3,10 +3,14 @@
 int analogPin0 = A0;
 int analogPin1 = A1;
 int analogPin2 = A2;
+int analogPin3 = A3;
+int analogPin4 = A4;
 
 int analogValue0 = 0;
 int analogValue1 = 0;
 int analogValue2 = 0;
+int analogValue3 = 0;
+int analogValue4 = 0;
 
 const float referenceVoltage = 5.0; // Adjust if using a different reference voltage
 ArduinoLEDMatrix matrix;
@@ -59,22 +63,30 @@ void setup() {
 }
 
 void loop() {
-  // Read analog inputs continuously
+  // put your main code here, to run repeatedly:
   analogValue0 = analogRead(analogPin0);
   analogValue1 = analogRead(analogPin1);
   analogValue2 = analogRead(analogPin2);
+  analogValue3 = analogRead(analogPin3);
+  analogValue4 = analogRead(analogPin4);
 
   // Convert analog readings to voltage
   float voltage0 = (analogValue0 * referenceVoltage) / 1023.0;
   float voltage1 = (analogValue1 * referenceVoltage) / 1023.0;
   float voltage2 = (analogValue2 * referenceVoltage) / 1023.0;
+  float voltage3 = (analogValue3 * referenceVoltage) / 1023.0;
+  float voltage4 = (analogValue4 * referenceVoltage) / 1023.0;
 
   // Print the analog values as voltage to the serial monitor
   Serial.print(voltage0);
   Serial.print(",");
   Serial.print(voltage1);
   Serial.print(",");
-  Serial.println(voltage2);
+  Serial.print(voltage2);
+  Serial.print(",");
+  Serial.print(voltage3);
+  Serial.print(",");
+  Serial.println(voltage4);
 
   // Handle LED matrix animation without blocking
   unsigned long currentTime = millis();
